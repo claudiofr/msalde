8417497b96bb28ad61efb1d3f96b922d902fa3d2
claudiofr <claudiofr@yahoo.com>
2025-08-08 15:08:31 -0400

    dev

--- msalde/plm.py
+++ msalde/plm.py
@@ -1,2 +1,15 @@
+from .model import Variant
+import numpy as np
+
+
 class PLMModel:
-    pass
+    def embed_sequences(self, sequences: list[str]) -> np.ndarray:
+        pass
+
+    def embed_variants(self, variants: list[Variant]) -> list[Variant]:
+        embeddings = self.embed_sequences([variant.sequence for variant in
+                                          variants])
+        vars = [Variant(id=var.id, sequence=var.sequence,
+                        embedding=embedding) for var, embedding in
+                zip(variants, embeddings)]
+        return vars
